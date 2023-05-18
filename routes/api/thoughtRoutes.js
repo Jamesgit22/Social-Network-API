0e11b949ae75b5ca48d3198065f7290de322ad10
Jamesgit22 <jschroeder97@icloud.com>
2023-05-18 16:35:52 -0500

    Added endpoint to get one thought by id from the database.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,20 +10,32 @@ router.get('/all-thoughts', async (req, res) => {
     console.log(
       'Unable to retrieve all thoughts from the database. Error: ' + err
     );
-    res
-      .status(500)
-      .json({
-        message:
-          'Something went wrong while retriving all thoughts from the database.',
-      });
+    res.status(500).json({
+      message:
+        'Something went wrong while retriving all thoughts from the database.',
+    });
   }
 });
 
 // Get : To get a single thought by _id.
+router.get('/thought:id', async (req, res) => {
+  try {
+    const results = await Thought.findById(req.params.id);
+    res.status(200).json(results);
+  } catch (err) {
+    console.log(
+      'Error in retrieving one thought by id from the database. Error: ' + err
+    );
+    res
+      .status(500)
+      .json({ message: 'Unable to retieve thought from the database.' });
+  }
+});
 
 // Post : To create a new thought. Ensure to push the new thoughts _id field to the associated users thought array.
 
 // Put : update a thought by _id.
+
 
 // Del : Delete a thought by _id.
 
