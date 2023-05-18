dde05af2f3ea21686efa254b33ee9725e12c290b
Jamesgit22 <jschroeder97@icloud.com>
2023-05-18 16:44:56 -0500

    Added endpoint for updating a thought by id in the database.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -18,7 +18,7 @@ router.get('/all-thoughts', async (req, res) => {
 });
 
 // Get : To get a single thought by _id.
-router.get('/thought:id', async (req, res) => {
+router.get('/thought/:id', async (req, res) => {
   try {
     const results = await Thought.findById(req.params.id);
     res.status(200).json(results);
@@ -35,8 +35,25 @@ router.get('/thought:id', async (req, res) => {
 // Post : To create a new thought. Ensure to push the new thoughts _id field to the associated users thought array.
 
 // Put : update a thought by _id.
-
+router.put('/update-thought/:id', async (req, res) => {
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      req.params.id,
+      { thoughtText: req.body.thoughtText },
+      { new: true }
+    );
+    res.status(200).json(updatedThought);
+  } catch (err) {
+    console.log(
+      'Error in updating a thought by id in the database. Error: ' + err
+    );
+    res
+      .status(500)
+      .json({ message: 'Unable to update thought in the database.' });
+  }
+});
 
 // Del : Delete a thought by _id.
+
 
 module.exports = router;
